f4efee0e005608815fe4719c78d4b69d9d36cd44
root <root@localhost.localdomain>
2019-03-25 23:49:29 +0300

    expalin sh "echo"

--- install.sh
+++ install.sh
@@ -5,12 +5,18 @@
 
 
 # start install:
+echo "sudo apt-get update"
 sudo apt-get update
+
+echo "installing/updatin tor"
 sudo apt-get install tor
+
+echo "installing/updatin pip"
 sudo apt-get install python-pip
 
 #pip requirements:
 pip install --upgrade -r requirements.txt
 
 # extension:
+echo "installing extensions:"
 git clone https://github.com/Mebus/cupp.git
